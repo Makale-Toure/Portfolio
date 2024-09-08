06a90f894988470704048550c37c954249372bc1
Makalé Touré <makale.toure03@gmail.com>
2024-09-08 13:58:25 +0200

    version reviewed

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
-// next.config.js
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '';
-let basePath = '/Portfolio';
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-}
-
-const nextConfig = {
-    output: 'export',
-    assetPrefix: assetPrefix,
-    basePath: basePath,
-    trailingSlash: true,
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;

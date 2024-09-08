e394b08a38ebfe2e3d8193f99dcd29c748385078
Makalé Touré <makale.toure03@gmail.com>
2024-09-08 16:01:28 +0200

    newBuild

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -10,6 +10,7 @@ export const Contact = () => {
         <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
             I will be honored to work with you <Smile className="inline" size={32}></Smile> !
         </h2>
+
         <div className="flex max-md:flex-col w-full gap-4">
             <ContactCard
                 className="flex-1"

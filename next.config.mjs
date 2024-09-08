6064dec377c57f631a20838360d7d7dd9eb68b12
Makalé Touré <119361788+Makale-Toure@users.noreply.github.com>
2024-09-08 12:45:48 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-export default nextConfig;
+let assetPrefix = '';
+let basePath = '/Portfolio';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  trailingSlash: true,
+};
